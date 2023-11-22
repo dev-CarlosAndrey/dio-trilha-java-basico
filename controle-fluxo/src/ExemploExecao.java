91a4f3fd5d2467a55b06207e5b48a0c5c80ba2b8
dev-CarlosAndrey <andreybezerra.info@gmail.com>
2023-11-22 13:33:46 -0300

    retirei-exemplo

--- controle-fluxo/src/ExemploExecao.java
+++
@@ -1,8 +0,0 @@
-import java.text.NumberFormat;
-
-public class ExemploExecao {
-    public static void main(String[] args) {
-        Number valor = NumberFormat.getInstance().parse("a1.75"); 
-        System.out.println(valor);
-    }
-}
