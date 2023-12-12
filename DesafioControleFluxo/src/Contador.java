e13d68f4a775c00fdbdc1acfc068f0160f2febc0
dev-CarlosAndrey <andreybezerra.info@gmail.com>
2023-12-12 12:58:55 -0300

    commit final desafio

--- DesafioControleFluxo/src/Contador.java
+++ DesafioControleFluxo/src/Contador.java
@@ -1,31 +1,32 @@
 import java.util.Scanner;
 public class Contador {
     public static void main(String[] args) {
-        
-		// Digitação dos numeros via terminal 
-		Scanner scanner = new Scanner (System.in);
-        int paramentroUm = scanner.nextInt();
-        int paramentroDois = scanner.nextInt();
+        // Digitação dos numeros via terminal 
+	Scanner scanner = new Scanner (System.in);
+    int paramentroUm = scanner.nextInt();
+    int paramentroDois = scanner.nextInt();
 		
-		try {
-            contar(paramentroUm, paramentroDois);
-        } catch (ParametrosInvalidosException error) {
-            System.err.println("Error: " + error.getMessage());
+	try {
+        contar(paramentroUm, paramentroDois);
+    } catch (ParametrosInvalidosException error) {
+        System.err.println("Error: " + error.getMessage());
         }
     }
 
     
-    static void contar (int paramentroUm, int paramentroDois) throws ParametrosInvalidosException {
+static void contar (int paramentroUm, int paramentroDois) throws ParametrosInvalidosException {
 
-        if (paramentroDois<paramentroUm) {
-            throw new ParametrosInvalidosException("O segundo paramentro deve ser maior que o primeiro");
-        }
+    if (paramentroDois<paramentroUm) {
+        throw new ParametrosInvalidosException("O segundo paramentro deve ser maior que o primeiro");
+    }
 
-        int contagem = paramentroDois - paramentroUm; 
+    int contagem = paramentroDois - paramentroUm; 
 
-        for (int i = contagem; i<=paramentroDois; i++) {
-            System.out.println("Imprimindo o número " + i);
-        }
+    for (int i = contagem; i<=paramentroDois; i++) {
+        
+        System.out.println("Imprimindo o número " + i);
+        
+    }
     
 }
     
